843c5e33d8cbac9d9f76ae3d2c82002d343a1f0d
qsaleh <qmsaleh@gmail.com>
2020-01-14 20:57:53 +0000

    created spinner1.js with Hayder and Thiago

---
+++ spinner1.js
@@ -0,0 +1,12 @@
+process.stdout.write('hello from spinner1.js... \rheyyy\n');
+const vTxt = "|/-\|/-\\|"
+let delay = 50;
+for (const char of vTxt) {
+  setTimeout(() => {
+    process.stdout.write("\r" + char);
+  }, delay);
+  delay += 200;
+}
+setTimeout (() =>{
+  process.stdout.write('\n')
+}, delay += 100);
